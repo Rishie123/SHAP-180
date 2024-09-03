d179f9a127425bfae301afda0a813c1741d1faf8
Rishabh Jain <88618109+Rishie123@users.noreply.github.com>
2024-09-03 15:40:50 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import pandas as pd
 
 # Load the combined DataFrame
-df = pd.read_csv('combined_feature_importance_shap_180.csv')
+df = pd.read_csv('combined_feature_importance_180.csv')
 
 # Initialize the Dash app
 app = dash.Dash(__name__)
