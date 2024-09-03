e5f05da4a5546394b7897825a90482369c8ea982
Rishabh Jain <88618109+Rishie123@users.noreply.github.com>
2024-09-03 15:51:28 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -25,12 +25,12 @@ def prepare_data(df, type_label):
 def create_figure(data, title):
     fig = px.area(data, title=title)
     fig.update_layout(
-        title={'text': title, 'font': {'size': 20}},
-        xaxis_title={'text': 'Time of Heater Profile (seconds) [Note: This is equivalent to Time after turning on the Heaters and switching it off at 60 seconds boundsay]', 'font': {'size': 16}},
-        yaxis_title={'text': 'Normalized Importance', 'font': {'size': 16}},
-        legend={'font': {'size': 14}},
-        xaxis={'tickfont': {'size': 12}},
-        yaxis={'tickfont': {'size': 12}}
+        title={'text': title, 'font': {'size': 22}},
+        xaxis_title={'text': 'Time of Heater Profile (seconds) [Note: This is equivalent to Time after turning on the Heaters and switching it off at 60 seconds boundsay]', 'font': {'size': 18}},
+        yaxis_title={'text': 'Normalized Importance', 'font': {'size': 18}},
+        legend={'font': {'size': 15}},
+        xaxis={'tickfont': {'size': 14}},
+        yaxis={'tickfont': {'size': 14}}
     )
     return fig
 
